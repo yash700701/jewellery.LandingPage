545d7b2e82a149d4794a13df928408fc0646e122
agrim08 <agrimgupta0805@gmail.com>
2024-12-14 11:47:01 +0530

    changes

--- jwel/src/Card.jsx/CategoryCard.jsx
+++ jwel/src/Card.jsx/CategoryCard.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
+import React from "react";
 
-function CategoryCard({data}) {
-    const { img, category } = data;
-    console.log(img);
-    
+function CategoryCard({ data }) {
+  const { img, category } = data;
+  console.log(img);
+
   return (
-    <div className='h-48 w-48 p-5'>
-     <img className='rounded-xl' src={`${img}`} alt="img"/>
-     <p>{category}</p>
+    <div className="h-48 w-48 p-5">
+      <img className="rounded-xl" src={`${img}`} alt="img" />
+      <p>{category}</p>
     </div>
-  )
+  );
 }
 
-export default CategoryCard
+export default CategoryCard;
